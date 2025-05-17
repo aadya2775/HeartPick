21cbb55bc27b57e46d0808516d6740fac8df43fc
aadya2775 <aadya2775@gmail.com>
2025-05-17 13:20:58 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+  <div className='bg-red-500'>
+    <h1>Hello World</h1>
+  </div>
+
+    </>
+  )
+}
+
+export default App
