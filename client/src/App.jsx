dbaa4dcbdac69dfb36a5aac181f7172aad5a26fa
Shubham Gupta <153124453+shubham-guptaaa@users.noreply.github.com>
2025-05-18 13:53:20 +0530

    app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import NavBar from './components/NavBar'
 import HeroSection from './components/HeroSection'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <NavBar />
       <HeroSection />
+      <Footer />
     </>
   )
 }
